4df58af193c7140ab870f15280354427c81edf96
Yi Kuo <kuokuoyiyi@gmail.com>
2025-04-01 14:20:24 +0800

    fix: change route name to singular as per REST specification

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -24,21 +24,21 @@ module LostNFound
 
       routing.on 'api' do
         routing.on 'v1' do
-          routing.on 'items' do
-            # GET api/v1/items/[uuid]
+          routing.on 'item' do
+            # GET api/v1/item/[uuid]
             routing.get String do |id|
               Item.find(id).to_json
             rescue StandardError
               routing.halt 404, { message: 'Item not found' }.to_json
             end
 
-            # GET api/v1/items
+            # GET api/v1/item
             routing.get do
               output = { item_ids: Item.all }
               JSON.pretty_generate(output)
             end
 
-            # POST api/v1/items
+            # POST api/v1/item
             routing.post do
               new_data = JSON.parse(routing.body.read)
               new_item = Item.new(new_data)
