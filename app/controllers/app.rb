87d27b6ac951798829277a5a845a601dca544119
Yi Kuo <kuokuoyiyi@gmail.com>
2025-04-01 14:17:18 +0800

    feat: web api

---
+++ app/controllers/app.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'roda'
+require 'json'
+
+require_relative '../models/item'
+
+module LostNFound
+  # Web controller for LostNFound API
+  class Api < Roda
+    plugin :environments
+    plugin :halt
+
+    configure do
+      Item.setup
+    end
+
+    route do |routing| # rubocop:disable Metrics/BlockLength
+      response['Content-Type'] = 'application/json'
+
+      routing.root do
+        { message: 'LostNFound API up at /api/v1' }.to_json
+      end
+
+      routing.on 'api' do
+        routing.on 'v1' do
+          routing.on 'items' do
+            # GET api/v1/items/[uuid]
+            routing.get String do |id|
+              Item.find(id).to_json
+            rescue StandardError
+              routing.halt 404, { message: 'Item not found' }.to_json
+            end
+
+            # GET api/v1/items
+            routing.get do
+              output = { item_ids: Item.all }
+              JSON.pretty_generate(output)
+            end
+
+            # POST api/v1/items
+            routing.post do
+              new_data = JSON.parse(routing.body.read)
+              new_item = Item.new(new_data)
+
+              if new_item.save
+                response.status = 201
+                { message: 'Item saved', id: new_item.uuid }.to_json
+              else
+                routing.halt 400, { message: 'Could not save item' }.to_json
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
