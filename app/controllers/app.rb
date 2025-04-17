bf82204b331abd012b51108f63e478979be37c83
Fifi <fifi.ding@iss.nthu.edu.tw>
2025-04-17 16:11:06 +0800

    feat: update controller logic

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -2,54 +2,89 @@
 
 require 'roda'
 require 'json'
-
-require_relative '../models/item'
+require 'logger'
 
 module LostNFound
   # Web controller for LostNFound API
   class Api < Roda
-    plugin :environments
     plugin :halt
 
-    configure do
-      Item.setup
-    end
-
-    route do |routing| # rubocop:disable Metrics/BlockLength
+    route do |routing|
       response['Content-Type'] = 'application/json'
 
       routing.root do
         { message: 'LostNFound API up at /api/v1' }.to_json
       end
+      @api_root = 'api/v1'
+      routing.on @api_root do
+        routing.on 'delegates' do
+          @dele_route = "#{@api_root}/delegates"
 
-      routing.on 'api' do
-        routing.on 'v1' do
-          routing.on 'item' do
-            # GET api/v1/item/[uuid]
-            routing.get String do |id|
-              Item.find(id).to_json
-            rescue StandardError
-              routing.halt 404, { message: 'Item not found' }.to_json
-            end
+          routing.on String do |dele_id|
+            routing.on 'items' do
+              @item_route = "#{@api_root}/delegates/#{dele_id}/items"
+              # GET api/v1/delegates/[dele_id]/items/[item_id]
+              routing.get String do |item_id|
+                item = Item.where(delegate_id: dele_id, id: item_id).first
+                item ? item.to_json : raise('item not found')
+              rescue StandardError => e
+                routing.halt 404, { message: e.message }.to_json
+              end
 
-            # GET api/v1/item
-            routing.get do
-              output = { item_ids: Item.all }
-              JSON.pretty_generate(output)
-            end
+              # GET api/v1/delegates/[dele_id]/items
+              routing.get do
+                output = { data: Delegate.first(id: dele_id).items }
+                JSON.pretty_generate(output)
+              rescue StandardError
+                routing.halt 404, message: 'Could not find documents'
+              end
 
-            # POST api/v1/item
-            routing.post do
-              new_data = JSON.parse(routing.body.read)
-              new_item = Item.new(new_data)
+              # POST api/v1/delegates/[ID]/items
+              routing.post do
+                new_data = JSON.parse(routing.body.read)
+                dele = Delegate.first(id: dele_id)
+                new_item = dele.add_item(new_data)
 
-              if new_item.save
-                response.status = 201
-                { message: 'Item saved', id: new_item.uuid }.to_json
-              else
-                routing.halt 400, { message: 'Could not save item' }.to_json
+                if new_item
+                  response.status = 201
+                  response['Location'] = "#{@item_route}/#{new_item.id}"
+                  { message: 'Item saved', data: new_item }.to_json
+                else
+                  routing.halt 400, 'Could not save item'
+                end
+              rescue StandardError
+                routing.halt 500, { message: 'Database error' }.to_json
               end
             end
+
+            # GET api/v1/delegates/[ID]
+            routing.get do
+              dele = Delegate.first(id: dele_id)
+              dele ? dele.to_json : raise('Delegate not found')
+            rescue StandardError => e
+              routing.halt 404, { message: e.message }.to_json
+            end
+          end
+
+          # GET api/v1/delegates
+          routing.get do
+            output = { data: Delegate.all }
+            JSON.pretty_generate(output)
+          rescue StandardError
+            routing.halt 404, { message: 'Could not find delegates' }.to_json
+          end
+
+          # POST api/v1/delegates
+          routing.post do
+            new_data = JSON.parse(routing.body.read)
+            new_dele = Delegate.new(new_data)
+            raise('Could not save delegate') unless new_dele.save_changes
+
+            response.status = 201
+            response['Location'] = "#{@dele_route}/#{new_dele.id}"
+            { message: 'Delegate saved', data: new_dele }.to_json
+          rescue StandardError => e
+            routing.halt 400, { message: e.message }.to_json
           end
         end
       end
