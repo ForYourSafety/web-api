372593ddb66c53616a0c22d9302aa94996a71d28
Yi Kuo <kuokuoyiyi@gmail.com>
2025-04-22 15:46:53 +0800

    feat: remove category specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,9 +10,7 @@ require_relative 'test_load_all'
 
 def wipe_database
   app.DB[:items].delete
-  app.DB[:categories].delete
 end
 
 DATA = {} # rubocop:disable Style/MutableConstant
 DATA[:items] = YAML.safe_load_file('db/seeds/item_seeds.yml')
-DATA[:categories] = YAML.safe_load_file('db/seeds/category_seeds.yml')
