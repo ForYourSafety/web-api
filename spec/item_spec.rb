c9b7b93a7ab6dc214a09ac6f5db066e61c6484c1
Yi Kuo <kuokuoyiyi@gmail.com>
2025-04-22 16:02:34 +0800

    test: single item detail

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -25,15 +25,17 @@ describe 'Test Item Handling' do
 
   it 'HAPPY: should be able to get details of a single item' do
     item_data = DATA[:items][1]
-    cate = LostNFound::Category.first
-    item = cate.add_item(item_data).save # rubocop:disable Sequel/SaveChanges
+    item_data['type'] = item_data['type'].to_sym # Convert string to enum
+    LostNFound::Item.create(item_data).save_changes
+    item = LostNFound::Item.first
 
-    get "/api/v1/categories/#{cate.id}/items/#{item.id}"
+    get "/api/v1/items/#{item.id}"
     _(last_response.status).must_equal 200
 
     result = JSON.parse last_response.body
     _(result['data']['attributes']['id']).must_equal item.id
-    _(result['data']['attributes']['itemname']).must_equal item_data['itemname']
+    _(result['data']['attributes']['name']).must_equal item_data['name']
+    _(result['data']['attributes']['type'].to_sym).must_equal item_data['type']
   end
 
   it 'SAD: should return error if unknown document requested' do
