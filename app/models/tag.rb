071727802f8773b93e8bc6c41be033244f7af3dd
Sarah-Pan <sarah2950@gmail.com>
2025-04-30 22:29:56 +0800

    feat: add tag's model

---
+++ app/models/tag.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'sequel'
+
+module LostNFound
+  # model for item tags
+  class Tag < Sequel::Model
+    many_to_many :items, class: 'LostNFound::Item', join_table: :items_tags
+
+    plugin :uuid, field: :id
+
+    plugin :timestamps, update_on_create: true
+    plugin :whitelist_security
+    set_allowed_columns :name
+
+    def to_json(options = {})
+      JSON({ data: { type: 'tag', attributes: { id:, name: } } }, options)
+    end
+  end
+end
