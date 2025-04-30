6e4a6af88e6f2b374df000a10daaccb1f61ff67d
Sarah-Pan <sarah2950@gmail.com>
2025-04-30 22:32:17 +0800

    feat: add item_tag table migration file

---
+++ db/migrations/004_items_tags_join_create.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:items_tags) do
+      foreign_key :item_id, :items, type: :uuid, on_delete: :cascade
+
+      foreign_key :tag_id, :tags, type: :uuid, on_delete: :cascade
+
+      primary_key %i[item_id tag_id]
+
+      DateTime :created_at
+    end
+  end
+end
