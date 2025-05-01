4c3d91def8bbc68376a225b60ceb3eab476ee43b
Yi Kuo <kuokuoyiyi@gmail.com>
2025-05-02 00:30:03 +0800

    fix: service add tag to item spec

--- spec/integration/service_add_tag_to_item.rb
+++ spec/integration/service_add_tag_to_item.rb
@@ -4,32 +4,30 @@ require_relative '../spec_helper'
 
 describe 'Test AddTagToItem service' do
   before do
-    account_data = DATA[:accounts].first.clone
-    @account = LostNFound::Account.create(account_data)
+    owner = LostNFound::Account.create(DATA[:accounts].first)
+    owner.save_changes
 
-    item_data = DATA[:items][1].clone
-    item_data['type'] = item_data['type'].to_sym
-    item_data['created_by'] = @account.id
-    LostNFound::Item.create(item_data).save_changes
-    @item = LostNFound::Item.first
+    @item = LostNFound::CreateItemForOwner.call(
+      owner_id: owner.id,
+      item_data: DATA[:items][1]
+    )
 
-    tag_data = DATA[:tags].first.clone
-    @tag_name = LostNFound::Tag.create(tag_data)
+    @tag = LostNFound::Tag.create(DATA[:tags][0])
+    @tag.save_changes
+  end
 
-    it 'HAPPY: should be able to add a tag to an item' do
-      LostNFound::AddTagToItem.call(item_id: @item.id, tag_name: @tag_name)
+  it 'HAPPY: should be able to add a tag to an item' do
+    LostNFound::AddTagToItem.call(item_id: @item.id, tag_id: @tag.id)
 
-      _(@item.tags.count).must_equal 1
-      _(@item.tags.first.name).must_equal @tag_name
-    end
+    _(@item.tags.count).must_equal 1
+    _(@item.tags.first.name).must_equal @tag.name
+  end
 
-    it 'HAPPY: should reuse existing tag if already exists' do
-      LostNFound::Tag.create(name: @tag_name)
+  it 'HAPPY: should reuse existing tag if already exists' do
+    LostNFound::AddTagToItem.call(item_id: @item.id, tag_id: @tag.id)
+    LostNFound::AddTagToItem.call(item_id: @item.id, tag_id: @tag.id)
 
-      LostNFound::AddTagToItem.call(item_id: @item.id, tag_name: @tag_name)
-
-      _(LostNFound::Tag.where(name: @tag_name).count).must_equal 1
-      _(@item.tags.first.name).must_equal @tag_name
-    end
+    _(LostNFound::Tag.where(name: @tag.name).count).must_equal 1
+    _(@item.tags.first.name).must_equal @tag.name
   end
 end
